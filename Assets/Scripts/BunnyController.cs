f93c07f9c4384d5b26a064de38a948b7d6a62c1e
Joshua Rogers <Joshua.Rogers@iam.local>
2018-11-07 15:23:14 -0600

    Animator, Movement, and Ground Check
    -Added Idle animation
    -Added Skip animation
    -Added player movement
    -Added a boolean to check if player is on the ground

---
+++ Assets/Scripts/BunnyController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyController : MonoBehaviour
+{
+    
+    public float maxSpeed = 10f;
+    bool facingRight = true;
+
+    Animator anim;
+
+    bool grounded = false;
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+
+    public float jumpForce = 700;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+
+        Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Ground", grounded);
+
+        anim.SetFloat("vSpeed", rb2d.velocity.y);
+
+        float move = Input.GetAxis("Horizontal");
+        anim.SetFloat("Speed", Mathf.Abs(move));     
+        rb2d.velocity= new Vector2(move * maxSpeed, rb2d.velocity.y);
+
+        if(move>0&&!facingRight)
+        {
+            Flip();
+        }
+
+        else if(move<0&&facingRight)
+        {
+            Flip();
+        }
+	}
+    private void Update()
+    {
+        Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
+        if (grounded&&Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("Ground", false);
+            rb2d.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
